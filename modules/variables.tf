4756c88f8ffeccb367b89abc78f728f35df22565
Cassio Moreira <moreira@hp.com>
2022-03-13 22:45:26 -0300

    make minor changes

---
+++ modules/variables.tf
@@ -0,0 +1,23 @@
+variable "kind_cluster_name" {
+  type        = string
+  description = "Cluster Name"
+  default     = "moreira-kind-cluster"
+}
+
+variable "kind_cluster_config_path" {
+  type        = string
+  description = "local config"
+  default     = "~/.kube/config"
+}
+
+variable "ingress_nginx_helm_version" {
+  type        = string
+  description = "Helm version for the nginx ingress controller."
+  default     = "4.0.6"
+}
+
+variable "ingress_nginx_namespace" {
+  type        = string
+  description = "The nginx namespace name"
+  default     = "ingress-nginx"
+}
